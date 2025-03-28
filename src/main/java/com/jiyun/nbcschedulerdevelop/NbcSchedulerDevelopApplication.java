45f9c56b4bdf823a626aff4747088881672cadcf
jiyun-im-dev <jiyun.im.dev@gmail.com>
2025-03-28 20:24:02 +0900

    initial commit

---
+++ src/main/java/com/jiyun/nbcschedulerdevelop/NbcSchedulerDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.jiyun.nbcschedulerdevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcSchedulerDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbcSchedulerDevelopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiyun/nbcschedulerdevelop/NbcSchedulerDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiyun.nbcschedulerdevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbcSchedulerDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
